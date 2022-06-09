ed68179b1c55c116935be8d3599a19560780d82e
Tom Tournillon <tom.tournillon@gmail.com>
2022-06-09 18:02:51 +0200

    Correction d'un guillemet

--- front/javascript/productList.js
+++ front/javascript/productList.js
@@ -7,7 +7,7 @@ fetch("http://localhost:3000/api/products/")
             `<a href=".product.html?id=${products[product].id}">
                 <article>
                     <img src="${products[product].imageUrl}" alt="${products[product].altTxt}">
-                    <h3 class="ProductName'>${products[product].name}</h3>
+                    <h3 class="ProductName">${products[product].name}</h3>
                     <p class="productDescription">${products[product].description}</p>
                 </article>
             </a>`
