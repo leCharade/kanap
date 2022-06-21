22f60c21d07b561a7a1bb1bf1f1d70e1c5cd5d41
Tom Tournillon <tom.tournillon@gmail.com>
2022-06-21 14:33:58 +0200

    Ajout des éléments au panier via LocalStorage

--- front/js/product.js
+++ front/js/product.js
@@ -21,3 +21,71 @@ fetch(`http://localhost:3000/api/products/${productId}`)
             .innerHTML += `<option value="${product.colors[color]}">${product.colors[color]}</option>`
         }
     })
+
+// Récupération de la quantité de canapés renseignée
+function quantityValue() {
+    let quantity = document.getElementById("quantity");
+    return quantity.value;
+}
+  
+// Récupération de la couleur choisie
+function colorValue() {
+    let color = document.getElementById("colors");
+    return color.value;
+}
+
+// Récupération des données du panier
+function getCart() {
+    let items = [];
+
+    // Si le panier existe, on remplit items avec le contenu du panier
+    if (localStorage.getItem("cart") != null) {
+      items = JSON.parse(localStorage.getItem("cart"));
+    }
+    return items;
+}
+
+// Fonction d'ajout des produits dans le panier
+function addToCart(productId, color, quantity) {
+
+    // Si pas de couleur ou de quantité renseignée, pas d'ajout au panier
+    if (quantity <= 0 || color == "") {
+      return;
+    }
+
+    // Si le panier est vide, on ajoute le produit avec la couleur et la quantité renseignée
+    let items = getCart();
+    if (items.length == 0) {
+      items = [[productId, color, quantity]];
+    }
+
+    // Sinon, on cherche dans le panier si un élément possède le même id et la même couleur que l'élément ajouté.
+    else {
+      let found = false;
+      for (let i = 0; i < items.length; i++) {
+
+        // Si l'élément correspondant est trouvé, on ajoute la quantité à la valeur de l'élément du panier.
+        if (productId === items[i][0] && color === items[i][1]) {
+          found = true;
+          items[i][2] += quantity;
+        }
+      }
+
+      // Si aucun élément correspondant n'est trouvé, on ajoute un nouvel élément.
+      if (found == false) {
+        let item = [productId, color, quantity];
+        items.push(item);
+      }
+    }
+
+    // Enfin, on convertit le résultat de la fonction en format JSON et on l'ajoute au panier.
+    localStorage.setItem("cart", JSON.stringify(items));
+}
+
+// On permet au bouton Ajouter au panier d'exécuter la fonction addToCart lorsqu'il est cliqué.
+const addToCartBtn = document.getElementById("addToCart");
+addToCartBtn.addEventListener("click", () => {
+    let quantity = parseInt(quantityValue());
+    let color = colorValue();
+    addToCart(productId, color, quantity);
+});
